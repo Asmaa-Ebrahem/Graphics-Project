023dbc32dcec169cbdd952f6e1b25da5503aae4b
EmyMahmoud <37907646+EmyMahmoud@users.noreply.github.com>
2018-04-15 12:35:36 +0200

    Level1-animationFood

---
+++ Assets/Foods/Scripts/Food.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+	public GameObject[] Foods;
+	// Use this for initialization
+	void Start () {
+		int RandomNumber = Random.Range (0, Foods.Length);
+		Foods [RandomNumber].SetActive (true);
+		for (int i = 0; i < Foods.Length; i++) 
+		{
+			if (i != RandomNumber) {
+				Foods [i].SetActive (false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
